0b1c40fb69291a8646ee8756598f8bb358929b87
shaiksohelll <shaiksohelll05@gmail.com>
2026-06-13 20:38:52 +0530

    fix(client): disable react-refresh rule in entry file main.jsx

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-refresh/only-export-components */
 import React, { lazy } from "react";
 import ReactDOM from "react-dom/client";
 import { ClerkProvider } from "@clerk/clerk-react";
